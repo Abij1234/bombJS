09b9dec949e01fb1d1293cb4114aad1cadc70d79
Suman Kumar ~BHUTUU <raj259942@gmail.com>
2022-02-19 18:11:55 +0530

    fixed an issue in bomber script

--- assets/bomb/bomber.sh
+++ assets/bomb/bomber.sh
@@ -8,13 +8,13 @@ signal_on_SIGINT() {
     exit 1
 }
 trap signal_on_SIGINT SIGINT
-if [ -f $CWD/apis ]; then
-    rm -rf $CWD/apis >/dev/null 2>&1
+if [ -f $CWD/api ]; then
+    rm -rf $CWD/api >/dev/null 2>&1
 fi
-cp -r $CWD/api/apis $CWD
+cp -r $CWD/apiAssets/api $CWD
 while read -r A; do
     echo "${A//€BHUTUU/$Number}"
-done < $CWD/apis > $CWD/api2
+done < $CWD/api > $CWD/api2
 rm -rf $CWD/api >/dev/null 2>&1
 while read -r X; do
     echo "${X//+91/$countryCode}"
